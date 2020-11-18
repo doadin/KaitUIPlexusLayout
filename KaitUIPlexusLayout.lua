4352fca05fddf6e8496444990f60b8ad3be4b786
doadin <tbkizle@gmail.com>
2020-11-18 17:49:59 -0500

    Create KaitUIPlexusLayout.lua

---
+++ KaitUIPlexusLayout.lua
@@ -0,0 +1,11 @@
+local PlexusLayout = Plexus:GetModule("PlexusLayout")
+
+PlexusLayout:AddLayout("Kait Single Column", {
+	[1] = {
+		groupFilter = "1,2,3,4,5,6,7,8",
+		groupBy = "CLASS",
+		groupingOrder = "HUNTER,WARRIOR,DEATHKNIGHT,ROGUE,MONK,PALADIN,DRUID,SHAMAN,PRIEST,MAGE,WARLOCK",
+		unitsPerColumn = 40,
+		maxColumns = 1,
+	},
+})
